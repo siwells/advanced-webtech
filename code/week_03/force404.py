33f8aaaaf068907513099437aae93c3a8e46c3b7
Simon Wells <siwells@gmail.com>
2015-10-04 23:05:43 +0100

    Force 404 error example

---
+++ code/week_03/force404.py
@@ -0,0 +1,17 @@
+from flask import Flask, abort
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/force404")
+def force404():
+    abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Couldn't find the page you requested", 404
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
