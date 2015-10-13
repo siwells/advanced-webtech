69c81d7c834a807028fe79860e9e4c5bd3f187ca
Simon Wells <siwells@gmail.com>
2015-10-13 17:01:58 +0100

    Update & simplify config example

--- code/week_06/config.py
+++ code/week_06/config.py
@@ -2,20 +2,39 @@ import ConfigParser
 
 from flask import Flask
 
-import configuration
-
 app = Flask(__name__)
 
-configuration.init(app)
 
 @app.route('/')
 def root():
-        return "Hello Napier from the configuration testing app" 
+  return "Hello Napier from the configuration testing app"
+
+@app.route('/config/')
+def config():
+  str = []
+  str.append('Debug:'+app.config['DEBUG'])
+  str.append('port:'+app.config['port'])
+  str.append('url:'+app.config['url'])
+  str.append('ip_address:'+app.config['ip_address'])
+  return '\t'.join(str)
+
+
+def init(app):
+    config = ConfigParser.ConfigParser()
+    try:
+        config_location = "etc/config.cfg"
+        config.read(config_location)
+        
+        app.config['DEBUG'] = config.get("config", "debug")
+        app.config['ip_address'] = config.get("config", "ip_address")
+        app.config['port'] = config.get("config", "port")
+        app.config['url'] = config.get("config", "url")
+    except:
+        print "Could not read configs from: ", config_location
 
 if __name__ == '__main__':
+    init(app)
     app.run(
         host=app.config['ip_address'], 
-        port=int(app.config['port']), 
-        threaded=app.config['threaded'])
-
+        port=int(app.config['port']))
 
