6abfeba32230080b6bb4d45f120c0b67d009def2
Simon Wells <siwells@gmail.com>
2015-10-21 19:07:26 +0100

    Use unstyled html template

--- code/week_07/unstyled.py
+++ code/week_07/unstyled.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-  return render_template('base.html'), 200
+  return render_template('unstyled.html'), 200
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
