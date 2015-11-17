b8c238fb9b3c5c2cd0cbb6e97613d6a2db44c51c
Simon Wells <siwells@gmail.com>
2015-11-17 09:23:41 +0000

    Add credential checking example

---
+++ code/week.09/login.py
@@ -0,0 +1,52 @@
+import bcrypt
+from functools import wraps
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+user = 'person@napier.ac.uk'
+pwhash = bcrypt.hashpw('secretpass', bcrypt.gensalt())
+
+def check_auth(email, password):
+    if(email == user and 
+        pwhash == bcrypt.hashpw(password.encode('utf-8'), pwhash)):
+            return True
+    return False
+
+def requires_login(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        status = session.get('logged_in', False)
+        if not status:
+            return redirect(url_for('.root'))
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/logout/')
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('.root'))
+
+@app.route("/secret/")
+@requires_login
+def secret():
+    return "Secret Page"
+
+@app.route("/", methods=['GET', 'POST'])
+def root():
+    if request.method == 'POST':
+        user = request.form['email']
+        pw = request.form['password']
+        print user, pw
+        print pwhash
+        
+        if check_auth(request.form['email'], request.form['password']):
+            session['logged_in'] = True
+            return redirect(url_for('.secret'))
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
