cf98091dd3e1361ef0b8532e4d029914bf877a28
Simon Wells <siwells@gmail.com>
2021-11-25 13:14:17 +0000

    Example to serve up HTML

---
+++ code/topic_12/simplest+html.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST, PORT = '', 8080
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print(f'Serving HTTP on port {PORT} ...')
+while True:
+    client_connection, client_address = listen_socket.accept()
+    request_data = client_connection.recv(1024).decode('utf-8')
+    print(request_data)
+
+    http_response = "HTTP/1.1 200 OK\n\n<h1>Hello Napier!</h1>".encode() 
+    client_connection.sendall(http_response)
+    client_connection.close()
+listen_socket.close()
+
