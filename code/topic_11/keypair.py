e19deea61d2099f685f107c89f03f69298d31f31
Simon Wells <siwells@gmail.com>
2021-11-26 18:27:35 +0000

    Add keypair generation example

---
+++ code/topic_11/keypair.py
@@ -0,0 +1,12 @@
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(2048)
+private_key = key.export_key()
+file_out = open("private.pem", "wb")
+file_out.write(private_key)
+file_out.close()
+
+public_key = key.publickey().export_key()
+file_out = open("receiver.pem", "wb")
+file_out.write(public_key)
+file_out.close()
